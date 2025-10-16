45f12e78c92d4f46201d2c71efb68274734753fc
MarlinSoe <aungnyeinchan.conceptx@gmail.com>
2025-10-16 09:37:34 +0630

    api baseURL update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:5001/api"
+// const BASE_URL = "http://localhost:5001/api"
+
+// const api = axios.create({
+//     baseURL: BASE_URL, 
+// })
+
+// export default api
+
+const BASE_URL = "https://chat-app-backend-itee.onrender.com/api";
 
 const api = axios.create({
-    baseURL: BASE_URL, 
+    baseURL: BASE_URL,
 })
 
-export default api
+export default api;
