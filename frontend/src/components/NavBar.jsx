76ff3bd954df56ba299b118a3674ef89b1061461
MarlinSoe <aungnyeinchan.conceptx@gmail.com>
2025-10-16 11:06:53 +0630

    mobile compatibility

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,33 +1,57 @@
-import '../styles/NavBar.css'
+import '../styles/NavBar.css';
 import NavBarContact from './NavBar/NavBarContact';
 import NavBarProfile from './NavBar/NavBarProfile';
 import NavBarSearch from './NavBar/NavBarSearch';
-import LogoutButton from './LogoutButton'
-import { useEffect, useState } from 'react';
-import api from '../lib/axios'
+import LogoutButton from './LogoutButton';
+import { useState } from 'react';
+import icon from '../assets/menu-burger.svg'
 
+function NavBar({
+    user, myUsername, myBio, myInfo, setMyInfo,
+    setMessages, setCurrentFriend, currentFriend,
+    setShowSendMessage, messages
+}) {
+    const [collapsed, setCollapsed] = useState(true);
 
-function NavBar({user, myUsername, myBio, myInfo, setMyInfo, setMessages, setCurrentFriend, currentFriend, setShowSendMessage, messages}) {
-    
+    const toggleNav = () => {
+        setCollapsed(prev => !prev);
+    };
 
-
-    // user contain token
     return (
         <>
-            <div className="nav-bar-wrapper">
-                
+            {/* Toggle Button (visible only on mobile) */}
+            <button 
+                className="nav-toggle-button"
+                onClick={toggleNav}
+            >
+                 <img src={icon} alt="" />
+            </button>
+
+            <div className={`nav-bar-wrapper ${collapsed ? 'collapsed' : ''}`}>
                 <NavBarProfile myInfo={myInfo}/>
-                <NavBarSearch user={user} myUsername={myUsername} myBio={myBio} myInfo={myInfo} setMyInfo={setMyInfo} setMessages={setMessages}/>
-                {Object.keys(myInfo).length > 0 && <NavBarContact myInfo={myInfo} user={user} setMessages={setMessages} setCurrentFriend={setCurrentFriend}  currentFriend={currentFriend} setShowSendMessage={setShowSendMessage} messages={messages}/>}
-                <LogoutButton/>
-                
-
-
-
+                <NavBarSearch
+                    user={user}
+                    myUsername={myUsername}
+                    myBio={myBio}
+                    myInfo={myInfo}
+                    setMyInfo={setMyInfo}
+                    setMessages={setMessages}
+                />
+                {Object.keys(myInfo).length > 0 &&
+                    <NavBarContact
+                        myInfo={myInfo}
+                        user={user}
+                        setMessages={setMessages}
+                        setCurrentFriend={setCurrentFriend}
+                        currentFriend={currentFriend}
+                        setShowSendMessage={setShowSendMessage}
+                        messages={messages}
+                    />
+                }
+                <LogoutButton />
             </div>
-
         </>
     );
 }
 
-export default NavBar
+export default NavBar;
